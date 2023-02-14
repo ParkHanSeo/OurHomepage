af2d178f859c5a3917233ec9c3e717a0ea834384
안재용 <dksodyd@gmail.com>
2023-02-14 15:54:02 +0900

    페이지업 모든 화면 적용 / 자잘한 오타수정

--- Ecommerce Platforms/src/main/webapp/resources/user/js/script.js
+++ Ecommerce Platforms/src/main/webapp/resources/user/js/script.js
@@ -699,7 +699,7 @@ function eduMemberFaq() {
 }
 
 function videoHeight() {
-	var $obj = $(".video-wrapper");
+	var $obj = $(".video-wrapper");d
 
 	function autoHeight() {
 		var $winH = $(window).outerHeight(),
@@ -757,7 +757,7 @@ function btnScrollTop() {
 			$btn.removeClass("is-end");
 		}
 
-		if ($scrT >= $winH / 2 && $(window).width() >= 1024) {
+		if ($scrT >= $winH / 2 && $(window).width() >= 0) {
 			$btn.fadeIn(300);
 		} else {
 			$btn.fadeOut(300);
